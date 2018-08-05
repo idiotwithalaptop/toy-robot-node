a71c94e21dbf11e6bbb66f20a037f838f00e9a46
idiotwithalaptop <piston47@gmail.com>
2018-08-06 09:19:56 +1000

    Added documentation for Table and removed redundant field

--- src/model/Table.js
+++ src/model/Table.js
@@ -1,7 +1,21 @@
+'use strict'
+
+/*
+ * AUTHOR: Ryan Brown
+ *
+ * Table class that represents the table that a robot is running on.
+ * The table is really the environment around the robot, which is fairly
+ * straight forward and doesn't provide much more functionality than
+ * maintaining the diameters of the table.  However, in the event that
+ * any obstacles were introduced, this would be the place to do it.
+ *
+ * Attributes available are:
+ *   width - The width of the table
+ *   height - The height of the table
+ */
 module.exports = class Table {
   constructor (width, height) {
     this.width = width
     this.height = height
-    this.robot = null
   }
 }
